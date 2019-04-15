22613ba221d852bc77697099629836b798fde8e6
Stefan Rieger <stefan.rieger@ooose.de>
2019-04-15 11:18:57 +0200

    Added configure method that was mistakenly removed

--- DataServer/src/main/java/de/oose/config/SecurityConfig.java
+++ DataServer/src/main/java/de/oose/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -20,17 +19,13 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	/**
 	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
 	 */
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		var keyCloakAuthProvider = keycloakAuthenticationProvider();
 		keyCloakAuthProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
 		auth.authenticationProvider(keyCloakAuthProvider);
 	}
-
 	
-	/**
-	 * Defines the session authentication strategy.
-	 */
 	@Bean
 	@Override
 	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -44,12 +39,8 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 		http
 			.csrf().disable()
 			.authorizeRequests().anyRequest().authenticated();
-	//		.authorizeRequests().anyRequest().authenticated()
-			//.and().requiresChannel().anyRequest().requiresSecure()
-			//.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	}
-	
-	
+		
 	@Bean
 	public KeycloakConfigResolver keyCloakConfigResolver() {
 		return new KeycloakSpringBootConfigResolver();
--- Resource/src/main/java/de/oose/config/SecurityConfig.java
+++ Resource/src/main/java/de/oose/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -20,8 +19,8 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	/**
 	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
 	 */
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		var keyCloakAuthProvider = keycloakAuthenticationProvider();
 		keyCloakAuthProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
 		auth.authenticationProvider(keyCloakAuthProvider);
